b85dd3d05ee80d2c1ee4eb910f8ec47f097f3e20
Juan Felipe Castillo Gomez <1006071586@u.icesi.edu.co>
2023-10-09 22:09:22 -0500

    Se agrego multihilos

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,6 +1,9 @@
 import java.io.*;
+import java.util.concurrent.*;
 
 public class Server {
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(10); // Crea un pool de 10 hilos
+
     public static void main(String[] args) {
         java.util.List<String> extraArgs = new java.util.ArrayList<String>();
 
@@ -12,10 +15,12 @@ public class Server {
                 }
             }
             com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapter("Printer");
-            com.zeroc.Ice.Object object = new PrinterI();
+            com.zeroc.Ice.Object object = new PrinterI(); // Aquí podrías tener lógica que requiera ejecución en un hilo separado.
             adapter.add(object, com.zeroc.Ice.Util.stringToIdentity("SimplePrinter"));
             adapter.activate();
             communicator.waitForShutdown();
+        } finally {
+            threadPool.shutdown(); // Asegúrate de cerrar el pool al salir.
         }
     }
 
@@ -36,5 +41,10 @@ public class Server {
             return "Error: " + ex.getMessage();
         }
         return output;
+    }
+
+    // Si necesitas ejecutar una tarea pesada en el futuro, utiliza algo similar a:
+    public static void executeHeavyTask(Runnable task) {
+        threadPool.submit(task);
     }
 }
